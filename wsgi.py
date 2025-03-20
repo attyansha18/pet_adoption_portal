2dc53fdc8afa9a533b97688c4ff81b9dea027345
Attyansha <as1637@srmist.edu.in>
2025-03-21 01:51:12 +0530

    modified

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pet_adoption.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pet-Adoption-portal.settings')
 
 application = get_wsgi_application()
-
-# Adding Whitenoise for serving static files
-application = WhiteNoise(application, root='/path/to/static/files')
